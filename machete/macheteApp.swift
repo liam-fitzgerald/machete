850ca2fa43f2c612f2b42709c5971dcd344a201b
Liam Fitzgerald <liam.fitzgerald@urbit.org>
2024-03-06 10:57:42 -0500

    Initial Commit

---
+++ machete/macheteApp.swift
@@ -0,0 +1,17 @@
+//
+//  macheteApp.swift
+//  machete
+//
+//  Created by Liam Fitzgerald on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct macheteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
